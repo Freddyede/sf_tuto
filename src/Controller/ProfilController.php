0a07776154e42be545b460e395cb31c2b8d0fcb2
Freddyede <f.patouillard@it-students.fr>
2022-11-13 16:34:47 +0100

    add secu

---
+++ src/Controller/ProfilController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ProfilController extends AbstractController
+{
+    #[Route('/profil', name: 'profil')]
+    #[Security("is_granted('ROLE_ADMIN') and is_granted('ROLE_USER')")]
+    public function index(): Response
+    {
+        return $this->render('profil/index.html.twig', [
+            'controller_name' => 'ProfilController',
+        ]);
+    }
+}
